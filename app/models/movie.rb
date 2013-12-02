e4484996f6c58c42b92ef320fe0afa239669b4ba
RJ Dabbar <rjdabbar@gmail.com>
2013-12-02 14:50:41 -0500

    commit with validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,9 @@
 class Movie < ActiveRecord::Base
-  validates :title, presence: true, uniqueness: true
-  validates :description, presence: true
-  validates :year_released, presence: true
+  validates :title, presence: {:message => "Enter a title."}, uniqueness: {:message => "That movie already exists, please enter a different one."}
+  validates :description, presence: true, length: { minimum: 10}
+  validates :year_released, presence: true, numericality: {greater_than: 0}
+  validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  
 
 
   def self.search_for query
